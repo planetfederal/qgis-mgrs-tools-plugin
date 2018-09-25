516c30555da7b2c9f7b8dc7e7f9bc080c44e988a
Alexandre Neto <senhor.neto@gmail.com>
2018-09-25 10:59:21 +0100

    Adds manual tests for processing algorithms

--- mgrstools/tests/testerplugin.py
+++ mgrstools/tests/testerplugin.py
@@ -2,9 +2,11 @@
 # https://github.com/boundlessgeo/qgis-tester-plugin
 
 import os
-from qgis.core import QgsProject
+from qgis.core import QgsProject, QgsApplication
 from qgis.utils import plugins, iface
 from qgiscommons2.layers import loadLayer
+from processing.gui.MessageDialog import MessageDialog
+from processing.gui.AlgorithmDialog import AlgorithmDialog
 
 def functionalTests():
     try:
@@ -23,6 +25,26 @@ def functionalTests():
         if os.path.normpath(currentProjectFile) != os.path.normpath(projectFile):
             iface.addProject(projectFile)
 
+    def _executeProcessingAlgorithm(algName):
+        alg = QgsApplication.processingRegistry().createAlgorithmById(algName)
+        if not alg:
+            return
+
+        ok, message = alg.canExecute()
+        if not ok:
+
+            dlg = MessageDialog()
+            dlg.setTitle("Error executing algorithm")
+            dlg.setMessage("This algorithm cannot be run :-( </h3>\n{0}".format(message))
+            dlg.exec_()
+            return
+
+        dlg = alg.createCustomParametersWidget()
+        if not dlg:
+            dlg = AlgorithmDialog(alg)
+        dlg.show()
+        dlg.exec_()
+
     def _setTool():
         plugins["mgrstools"].setTool()
 
@@ -35,13 +57,24 @@ def functionalTests():
     mgrsMaptool.addStep("Click within the upper left polygon. Verify that the computed mrgrs coord starts with 02HLJ'", isVerifyStep=True)
     mgrsMaptool.addStep("Open a text editor and press 'Ctrl+V'. Confirm that the computed coordinate is pasted", isVerifyStep=True)
 
-    mgrsZoomPanel = Test("Test MRGS zoomtopanel")
+    mgrsZoomPanel = Test("Test MRGS zoom to panel")
     mgrsZoomPanel.addStep("Load test project", lambda: _loadTestProject("map_tools_test"))
     mgrsZoomPanel.addStep("Open panel", _zoomTo)
     mgrsZoomPanel.addStep("Type '02HLJ' and click on 'zoom to'. Verify it centers the view on to the lower left corner of the previously clicked polygon and adds a marker", isVerifyStep=True)
     mgrsZoomPanel.addStep("Click on 'remove marker' and verify it removes the marker", isVerifyStep=True)
 
-    return [mgrsMaptool, mgrsZoomPanel]
+    mgrsCreateMgrsPoints = Test("Test Create point layer from MRGS field")
+    mgrsCreateMgrsPoints.addStep("Load test project", lambda: _loadTestProject("create_points_from_mgrs_test"))
+    mgrsCreateMgrsPoints.addStep("Opening algorithm dialog. Run on the 'points_with_mgrs' layer using the MGRS field as M", lambda: _executeProcessingAlgorithm("mgrs:createpointslayerfrommgrstable"))
+    mgrsCreateMgrsPoints.addStep("Confirm that a layer is added to the project, which points are at the lower left corner of the grid", isVerifyStep=True)
+
+    mgrsAddMGRSField = Test("Test Add MRGS field algorithm")
+    mgrsAddMGRSField.addStep("Load test project", lambda: _loadTestProject("add_mgrs_field"))
+    mgrsAddMGRSField.addStep("Opening algorithm dialog. Run the algorithm using the 'points_no_mgrs' layer", lambda: _executeProcessingAlgorithm("mgrs:addmgrsfield"))
+    mgrsAddMGRSField.addStep("Open 'Output' layer's table of attributes. Confirm it contains a MGRS field, which values are compatible with the 'MGRS_compare' field.", isVerifyStep=True)
+
+
+    return [mgrsMaptool, mgrsZoomPanel, mgrsCreateMgrsPoints, mgrsAddMGRSField]
 
 
 def unitTests():
